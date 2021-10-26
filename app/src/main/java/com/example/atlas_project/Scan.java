113c262be553e267b9a72ccebad3281c92b4ead2
shraddhamulay09 <shraddhamulay09@gmail.com>
2021-10-26 12:05:28 +0530

    Front end updated

--- app/src/main/java/com/example/atlas_project/Scan.java
+++ app/src/main/java/com/example/atlas_project/Scan.java
@@ -9,8 +9,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.atlas_project.customViews.InitializeGraph;
 import com.example.atlas_project.customViews.InitializeMap;
 import com.example.atlas_project.customViews.Location;
@@ -20,7 +22,8 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class Scan extends Activity {
 
-    Button start ;
+    //Button start ;
+    ImageView qrscan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +33,13 @@ public class Scan extends Activity {
         InitializeGraph.getInstance(5 ,11) ;
         FetchItemList.getInstance(getString(R.string.url)) ;
         setContentView(R.layout.activity_scan);
-        start =( Button) findViewById(R.id.start) ;
+        //start =( Button) findViewById(R.id.start) ;
+        qrscan=(ImageView)findViewById(R.id.qrscan) ;
+        Glide.with(this).load(R.drawable.qrscanner).into(qrscan);
 
         Toast.makeText(Scan.this, "you are entered", Toast.LENGTH_SHORT).show();
 
-        start.setOnClickListener(new View.OnClickListener() {
+        qrscan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                ProgressDialog dialog = ProgressDialog.show(Scan.this, "",
---
+++ app/src/main/java/com/example/atlas_project/layoutformation.java
@@ -0,0 +1,14 @@
+package com.example.atlas_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layoutformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layoutformation);
+    }
+}
